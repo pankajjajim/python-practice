0576ebafc6083cd0a97fadc687f81b341c61272a
pankajjajim <pankajjajim064@gmail.com>
2026-06-22 22:26:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+1 for snake
+-1 for water
+0 for gun
+'''
+
+computer = -1
+youstr = input("Enter your choice: ")
+youDict = {"s": 1, "w":-1, "g":0}
+you = youDict[youstr]
+
+if(computer == -1 and you == 1):
+    print("you win!")
+elif(computer == 1 and you == 0):
+    print("You Lose!")
+elif(computer == 1 and you == -1):
+    print("You lose!")
+elif(computer == 1 and you == 0):
+    print("You win!")
+    
