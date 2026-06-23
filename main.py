05de8e2b7815a9d677c3ecf7b9316cc7df8ed625
pankajjajim <pankajjajim064@gmail.com>
2026-06-23 16:21:23 +0530

    Enhance game logic and user feedback
    Added reverse mapping for choices and improved output messages.

--- main.py
+++ main.py
@@ -7,14 +7,33 @@
 computer = -1
 youstr = input("Enter your choice: ")
 youDict = {"s": 1, "w":-1, "g":0}
+reverseDict = {1: "Snake", -1: "Water", 0: "Gun"}
+
 you = youDict[youstr]
 
-if(computer == -1 and you == 1):
-    print("you win!")
-elif(computer == 1 and you == 0):
-    print("You Lose!")
-elif(computer == 1 and you == -1):
-    print("You lose!")
-elif(computer == 1 and you == 0):
-    print("You win!")
-    
+print(f"You chose {reverseDict[you]}\nComputre chose {reverseDict[computer]}")
+
+if(computer == you):
+    print("Its a draw")
+
+else:
+    if(computer == -1 and you == 1):
+        print("you win!")
+    elif(computer == -1 and you == 0):
+        print("You Lose!")
+    elif(computer == 1 and you == -1):
+        print("You lose!")
+    elif(computer == 1 and you == 0):
+        print("You win!")
+    elif(computer == 0 and you == -1):
+        print("You win!")
+    elif(computer == 0 and you == 1):
+        print("You Lose!")
+
+    else:
+        print("Something went wrong!")         
+       
+
+
+
+
